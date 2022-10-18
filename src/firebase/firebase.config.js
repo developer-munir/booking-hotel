25796b3ab327645f3fbca2b2040ee6681c1f13be
developer-munir <devolopermunir@gmail.com>
2022-10-19 00:34:29 +0600

    basic setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA_slwV50JuY-xrw_nbGVq4g9FmZ1tJkE",
+  authDomain: "booking-hotel1.firebaseapp.com",
+  projectId: "booking-hotel1",
+  storageBucket: "booking-hotel1.appspot.com",
+  messagingSenderId: "731795874310",
+  appId: "1:731795874310:web:5c374494e2ccd0faef1987",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
