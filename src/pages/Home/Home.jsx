557bd25c8dbe2b1bd3f70e445c35b2cff33c7d01
developer-munir <devolopermunir@gmail.com>
2022-10-19 12:12:02 +0600

    add header banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <p>Home Components</p>
+            <Banner></Banner>
         </div>
     );
 };
