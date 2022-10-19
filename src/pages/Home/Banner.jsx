14b13901f5ba2edeb786514b1f281a584964281a
developer-munir <devolopermunir@gmail.com>
2022-10-19 12:31:30 +0600

    change banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import banner from "../../assets/hotel-banner2.avif";
+import TypeWriter from "react-typewriter";
 const Banner = () => {
   return (
-    <div>
+    <div className="font-serif">
       <div
         className="hero min-h-screen"
         style={{ backgroundImage: `url(${banner})` }}
@@ -10,11 +11,16 @@ const Banner = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">
+              <TypeWriter typing={1}>Booking Hotel</TypeWriter>
+            </h1>
+
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              <TypeWriter typing={1}>
+                Tending a cliff-hanging Grand Hotel In a country ravaged by
+                civil war. My heart as its only bellhop. My brain as its Chinese
+                cook
+              </TypeWriter>
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
